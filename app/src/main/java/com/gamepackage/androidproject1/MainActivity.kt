cc5bc46d623308d33d6619ef5d1bf4921fb08675
GalIsraeli <galisraeli910@gmail.com>
2025-04-10 13:38:42 +0300

    feat: Initial commit - Added game activity and layout
    This commit introduces the basic structure for the game. It includes:
    
    - GameActivity: An activity to host the game logic.
    - activity_game.xml: Layout file for the game screen, containing UI elements like score, lives, player spaceship, obstacles, and controls.
    - Relevant drawable resources: Images for the background, spaceship, obstacles, controls, and other UI elements.
    - Removed default test files.

--- app/src/main/java/com/gamepackage/androidproject1/MainActivity.kt
+++ app/src/main/java/com/gamepackage/androidproject1/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.gamepackage.androidproject1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.gamepackage.androidproject1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
